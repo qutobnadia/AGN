05e1d2b340f7d155292318ce21e4c0a0a10bb0af
qutobnadia <118085101+qutobnadia@users.noreply.github.com>
2023-06-29 11:47:33 -0400

    Add files via upload

--- Gas_Projection_June29.py
+++ Gas_Projection_June29.py
@@ -86,7 +86,8 @@ for fname in List_diir:
     Gas_Softening = data["PartType0"]["SmoothingLength"][:]
     
     # this is where I started adding additional parameters
-    ds = yt.load(Directory_path,unit_base=unit_base)
+    #ds = yt.load(Directory_path,unit_base=unit_base)
+    ds = yt.load(Directory_path,unit_base)
     ions_names=['H I','Mg II','C IV','N V','O VI', 'O VII', 'O VIII', 'Ne VIII']  ## Exploration!
     trident.add_ion_fields(ds, ions=ions_names, ftype="PartType0")
     O6_ion_Fraction = ds.all_data()[('gas', 'O_p6_ion_fraction')]
