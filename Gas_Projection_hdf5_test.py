b996d2694617efc55304b6ac10160a3801b5172b
qutobnadia <118085101+qutobnadia@users.noreply.github.com>
2023-07-10 12:58:10 -0400

    Update Gas_Projection_hdf5_test.py

--- Gas_Projection_hdf5_test.py
+++ Gas_Projection_hdf5_test.py
@@ -56,6 +56,7 @@ for uu in range(1):
 
     ds = yt.load(Dir_path, unit_base=unit_base)
     ds.derived_field_list
+    print(ds.derived_field_list)
   
     BH_Center = data["PartType5"]["Coordinates"][:]
     Gas_location = data["PartType0"]["Coordinates"][:] - BH_Center
@@ -65,16 +66,20 @@ for uu in range(1):
     ions_names=['H I','Mg II','C IV','N V','O VI', 'O VII', 'O VIII', 'Ne VIII']  ## Exploration!
     trident.add_ion_fields(ds, ions=ions_names, ftype="PartType0") #PartType0 is for GIZMO and GADGET 
 
-    Oxygen6_mass = ds.all_data()[('gas', 'O_p7_mass')].in_units('Msun')
+    mass = Gas_mass
+    Oxygen5_mass = ds.all_data()[('gas', 'O_p5_mass')].in_units('Msun') # should give Msun/kpc^2 
+    Oxygen6_mass = ds.all_data()[('gas', 'O_p6_mass')].in_units('Msun')
+    Oxygen7_mass = ds.all_data()[('gas', 'O_p7_mass')].in_units('Msun')
+    Magnesium2_mass = ds.all_data()[('gas', 'Mg_p1_mass')].in_units('Msun') # THIS IS A GUESS, REVISE! 
     
     NN = 100
     hh = Gas_Softening
 
-    Particles = sphviewer.Particles(Gas_location, Oxygen6_mass, hh)
+    Particles = sphviewer.Particles(Gas_location, Magnesium2_mass, hh)
     Scene = sphviewer.Scene(Particles)
     
     fig = plt.figure(1,figsize=(15,5))
-    fig.suptitle(r"Hot thermal jet with higher energy flux: Snapshot #100 with Gas_mass and O8_ion_Fraction", fontsize=17, x=0.5, y=1.5)
+    fig.suptitle(r"Hot thermal jet with higher energy flux: Snapshot #100 with MgII_ion_Fraction", fontsize=17, x=0.5, y=1.5)
     plt.subplots_adjust(top =1.8, bottom=0.2, hspace=0.3, wspace=0.3)
     
     ax1 = fig.add_subplot(131)
@@ -85,7 +90,7 @@ for uu in range(1):
 
     z = Gas_location[:,2] # MASK!!
     mask_z=np.abs(z)<5  # MASK!!
-    Particles1 = sphviewer.Particles(Gas_location[mask_z], Oxygen6_mass[mask_z], hh[mask_z]) # MASK!!
+    Particles1 = sphviewer.Particles(Gas_location[mask_z], Magnesium2_mass[mask_z], hh[mask_z]) # MASK!!
 
     Scene1 = sphviewer.Scene(Particles1) # MASK!! 
     Scene1.update_camera(r='infinity', t=0, p = 0, roll = 0, x = 0, y = 0, z = 0, vmin= 6.3, vmax= 7.4, extent=extendd) 
@@ -108,7 +113,7 @@ for uu in range(1):
 
     x = Gas_location[:,0] # MASK!!
     mask_x=np.abs(x)<5  #5 is the parameter that we can modify 
-    Particles2 = sphviewer.Particles(Gas_location[mask_x], Oxygen6_mass[mask_x], hh[mask_x]) 
+    Particles2 = sphviewer.Particles(Gas_location[mask_x], Magnesium2_mass[mask_x], hh[mask_x]) 
 
     Scene2 = sphviewer.Scene(Particles2) # MODIFIED!
     Scene2.update_camera(r='infinity', t=-90, p = -90, roll = 0, x = 0, y = 0, z = 0, vmin= 6.3, vmax= 7.4, extent= extendd)
@@ -133,7 +138,7 @@ for uu in range(1):
 
     y = Gas_location[:,1]
     mask_y=np.abs(y)<5  #5 is the parameter that we can modify 
-    Particles3 = sphviewer.Particles(Gas_location[mask_y], Oxygen6_mass[mask_y], hh[mask_y]) 
+    Particles3 = sphviewer.Particles(Gas_location[mask_y], Magnesium2_mass[mask_y], hh[mask_y]) 
 
     Scene3 = sphviewer.Scene(Particles3) # MODIFIED!
     Scene3.update_camera(r='infinity', t=90, p = 0, roll = -90, x = 0, y = 0, z = 0, vmin= 6.3, vmax= 7.4, extent= extendd)
