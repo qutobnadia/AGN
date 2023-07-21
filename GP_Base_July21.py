051eb5eea8c7e1de1f9f8700d8d568a565de4379
qutobnadia <118085101+qutobnadia@users.noreply.github.com>
2023-07-21 10:45:16 -0400

    Update GP_Base_July21.py

--- GP_Base_July21.py
+++ GP_Base_July21.py
@@ -58,7 +58,7 @@ unit_base = {'UnitLength_in_cm'         : 3.08568e+21,
 print("Checkpoint 0")
 
 # Define how to actaully call the file directory for each jet type 
-if jetType := "Precessing kinetic jet with lower energy flux":
+if jetType == "Precessing kinetic jet with lower energy flux":
       jet == "m12_mcvt_m2_10000_tor4_pr45_100Myr_lower"
 elif jetType == "Precessing kinetic jet with higher energy flux":
       jet == "m12_mcvt_m2_10000_tor4_pr45_100Myr"
@@ -70,7 +70,7 @@ elif jetType == "No Jet":
       jet == "m12_mcvt_default_64"
 elif jetType == "Cosmic ray jet with lower energy flux":
       jet == "m12_mcvt_m2_t7_3000_tor3_CR10_t4_lower"
-else jetType == "Cosmic ray jet with higher energy flux":
+else jetType == "Cosmic ray jet with higher energy flux" :
       jet == "m12_mcvt_m2_t7_3000_tor3_CR10_t4"
     
 Directory_path = '/n/holylfs05/LABS/hernquist_lab/AGN_Feedback_Fire/' + jet + '/output/'
